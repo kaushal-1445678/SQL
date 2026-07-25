2b0a2167a4a34f51ab720b6b567dba972bc61b74
kaushal-1445678 <kaushal730223@gmail.com>
2026-07-25 20:05:31 +0000

    learn day 1 of sql

---
+++ day1/day1.sql
@@ -0,0 +1,16 @@
+-- CREATE TABLE employees (
+--     id INTEGER PRIMARY KEY,
+--     name TEXT,
+--     salary INTEGER,
+--     department TEXT
+-- );
+
+-- INSERT INTO employees (id, name, salary, department) VALUES
+-- (1, 'Ravi', 50000, 'IT'),
+-- (2, 'Neha', 60000, 'HR'),
+-- (3, 'Aman', 45000, 'IT'),
+-- (4, 'Pooja', 70000, 'Sales'),
+-- (5, 'Karan', 55000, 'IT'),
+-- (6, 'Sita', 65000, 'Finance');
+
+SELECT name,salary FROM employees;
